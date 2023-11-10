9b8143ff6ff42fe29d5180c0a0033f32d596d7be
Ahmad Abou Amoun <aaboumaoun@gmail.com>
2023-11-10 12:55:22 +0200

    commit

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
-class tab:
+class Tab:
     def __init__(self ,name , url):
         self.name = name
         self.url = url
 
-class browser:
+class Browser:
     def __init__(self):
         self.tabs = []
+    def open(self):
+        name = input("please enter the name of the tab ")
+        url = input("please enter the URL of the page ")
+        tab = Tab(name,url)
+        self.tabs.append(tab)
+
+def main():
+    browser = Browser()
+    browser.open()
+main()
