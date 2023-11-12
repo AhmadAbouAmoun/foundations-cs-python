a01e6d0129bbaaca7bfe4d70e9b1f99cc5209da7
Ahmad Abou Amoun <aabouamoun@gmail.com>
2023-11-12 13:40:46 +0200

    commit

--- main.py
+++ main.py
@@ -4,13 +4,16 @@ import requests
 import validators
 from lxml import html
 import json
-import pathlib
+import sys
 
 
 def tabToDict(Tab):
     return {'name': Tab.name,
             'url': Tab.url,
             'nested_tabs': [tabToDict(nested_tabs) for nested_tabs in Tab.nested_tabs]}
+
+
+
 class Tab:
     def __init__(self, name, url, nested_tabs=None):
         self.name = name
@@ -89,21 +92,24 @@ class Browser:
         file_path=input("please enter the path that you want to save the file in ")
         if os.path.exists(file_path):
             #https://bobbyhadz.com/blog/python-input-file-path here is the explanation for the os library and why i used it here
-            complete_name = os.path.join(file_path, file_name + ".txt")#https://stackoverflow.com/questions/8024248/telling-python-to-save-a-txt-file-to-a-certain-directory-on-windows-and-mac this is were i got i learnt about os join
-            json_string = json.dumps(browser_dic)
+            complete_name = os.path.join(file_path, file_name )#https://stackoverflow.com/questions/8024248/telling-python-to-save-a-txt-file-to-a-certain-directory-on-windows-and-mac this is were i got i learnt about os join
+            json_string = json.dumps(browser_dict,indent=4)
             with open(complete_name, 'w') as file:
                 file.write(json.dumps(json_string))
         else:
             print('the path is not valid we will save it in the same directory in the current directory ')
-            json_string = json.dumps(browser_dic,encoding='utf-8')#https://youtu.be/jABj-SEhtBc?si=6NuPANZI72Asgz2- this is the video that helped me in options number 7 and 8
+            json_string = json.dumps(browser_dict,indent=4)#https://youtu.be/jABj-SEhtBc?si=6NuPANZI72Asgz2- this is the video that helped me in understanding the json
             with open(file_name, 'w') as file:
-             file.write(json.dumps(json_string,ensure_ascii=False,indent=4))
+             file.write(json.dumps(json_string))
 
-    def importFiles(self):
-        file_name=('please enter the name of the file that you want to open ')
-        file_path = input('please enter the files directory ')
-        if os.path.exists(file_path):
-            with open(file_name,'r') as f:
+
+
+    def importFile(self):
+        file_name=input('please enter the name of the file that you want to open ')
+        file_path = input("please enter file's directory ")
+        if(os.path.exists(file_path)):
+            complete_name=os.path.join(file_path, file_name)
+            with open(complete_name,'r') as f:
                 browser_dict = json.loads(f.read())
             print(browser_dict)
 def main():
