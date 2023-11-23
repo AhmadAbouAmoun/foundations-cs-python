21c8821fdbdc6ac3b820e1eae894213f604751c6
Ahmad Abou Amoun <aabouamoun@gmail.com>
2023-11-23 16:02:57 +0200

    commit

--- assignment4.py
+++ assignment4.py
@@ -74,6 +74,103 @@ class Stack:
             print(string1, ' is palindromic')
         else:
             print(string1, ' is not palindromic')
+
+class Student:
+    def __init__(self,midterm_grade,final_grade,good_attitude):
+        self.midterm_grade = midterm_grade
+        self.final_grade = final_grade
+        self.good_attitude = good_attitude
+        self.next=None
+
+class FSW_Interview:
+    def __init__(self):
+        self.head = None
+        self.size = 0
+    def isEmpty(self):
+        return self.head==None
+    def enqueue(self,midterm,final,attitude):
+        node=Student(midterm,final,attitude)
+        if self.size == 0:
+            self.head = node
+            self.size += 1
+        else:
+            if node.good_attitude == True and self.head == False:
+                node.next = self.head
+                self.head = node
+                self.size += 1
+            elif node.good_attitude == True and self.head == True:
+                if node.final_grade > self.head.final_grade:
+                    node.next = self.head
+                    self.head = node
+                    self.size += 1
+                elif node.final_grade == self.head.final_grade:
+                    if node.midterm_grade >= self.head.midterm_grade:
+                        node.next = self.head
+                        self.head = node
+                        self.size += 1
+                    else:
+                        current = self.head
+                        previous = current
+                        while current and current.good_attitude == True and node.final_grade <= current.final_grade and node.midterm_grade <= current.midterm_grade:
+                            previous = current
+                            current = current.next
+                        if current.good_attitude != True or current.final_grade < node.final_grade or current.midterm_grade < node.midterm_grade:
+                            previous.next = node
+                            node.next = current
+                            self.size += 1
+                        else:
+                            current.next = node
+                            node.next = None
+                else:
+                    current = self.head
+                    previous = current
+                    while current and current.good_attitude == True and current.final_grade > node.final_grade:
+                        previous = current
+                        current = current.next
+                        if current.final_grade == node.final_grade:
+                            if current.midterm_grade <=node.midterm_grade:
+                                previous.next = node
+                                node.next = current
+                                break
+                        if current.good_attitude != True and current.final_grade < node.final_grade:
+                            previous.next = node
+                            node.next = current
+                        else:
+                            current.next = node
+                            node.next = None
+                    self.size += 1
+            elif node.good_attitude == False and self.head == True:
+                current = self.head
+                previous = current
+                while current and current.good_attitude == True:
+                    previous = current
+                    current = current.next
+                if current.good_attitude != True:
+                    if node.final_grade > current.final_grade:
+                        previous.next = node
+                        node.next = current
+                        self.size += 1
+                    elif node.final_grade == current.final_grade:
+                        if node.midterm_grade >= current.midterm_grade:
+                            previous.next = node
+                            node.next = current
+                            self.size += 1
+                        else:
+                            current.next = node
+                            node.next = None
+            elif node.good_attitude == False and self.head == False:
+
+    def dequeue(self):
+        if self.size == 0:
+            print('it is already empty')
+        elif self.size == 1:
+            self.head = None
+            self.size -= 1
+        else:
+            current = self.head
+            self.head = self.head.next
+            current.next = None
+            self.size -= 1
 def main():
     ll = LinkedList()
     stack=Stack()
